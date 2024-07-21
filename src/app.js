8ab1245acc876063fe0a90aecb7b900fba5df5f8
kaushikt01 <kaushik_tiwari@hotmail.com>
2024-07-21 13:35:45 +0530

    fix(app.js): fix cookie parsing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./routes/routes');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
+app.use(cookieParser());
 const PORT = process.env.PORT || 3001;
 
 // Configure CORS
